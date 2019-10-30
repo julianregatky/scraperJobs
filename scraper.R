70e24943590b8730631a34735d2bafc7b296d7cd
Julian Regatky <julianregatky@gmail.com>
2019-10-29 23:19:17 -0300

    Update scraper.R

--- scraper.R
+++ scraper.R
@@ -1,7 +1,25 @@
+library(xml2)
 library(rvest)
+library(dplyr)
 
-html <- read_html("https://www.zonajobs.com.ar/ofertas-de-empleo-argentina.html?recientes=true")
-listado <- html_nodes(html,'div.aviso-no-sponsor')
-elementos <- html_nodes(listado,'div')
-titulos <- html_text(html_nodes(elementos,'h2.titulo-aviso'))
-links <- html_attr(html_node(html_nodes(elementos,'div.wrapper'),'a'),'href')
+limpiar_string <- function(string) { return(gsub("^\\s+|\\s+$","",string)) }
+
+continuar <- TRUE
+pagina <- 1
+while(continuar)
+{
+  html <- read_html(paste0("https://www.zonajobs.com.ar/ofertas-de-empleo-argentina-pagina-",pagina,".html?recientes=true"))
+  listado <- html_nodes(html,'div.aviso-no-sponsor')
+  elementos <- html_nodes(listado,'div')
+  #titulos <- html_text(html_nodes(elementos,'h2.titulo-aviso'))
+  links <- html_attr(html_node(html_nodes(elementos,'div.wrapper'),'a'),'href')
+  for(i in 1:length(links))
+  {
+    html <- read_html(paste0("https://www.zonajobs.com.ar",links[i]))
+    titulo <- limpiar_string(html_text(html_node(html,'h1.aviso_title')))
+    empresa <- limpiar_string(html_text(html_node(html,'span#empresa')))
+    
+    specs <- html_nodes(html_nodes(html,'div.aviso_specs'),'div.spec_def')
+    
+  }
+}
