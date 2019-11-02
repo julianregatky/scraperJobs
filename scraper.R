4d47f19875c9ed6601e35206df13858a92513186
Julian Regatky <julianregatky@gmail.com>
2019-11-02 17:23:36 -0300

    Update scraper.R

--- scraper.R
+++ scraper.R
@@ -3,9 +3,9 @@ library(rvest)
 library(dplyr)
 library(mongolite)
 
-# Las noticias en la página van a estar ordenadas de más
-# reciente a más antigua. El scraper va a ir recorriendo
-# hasta que supere el límite de días 'max_dias'.
+# Los anuncios en las páginas van a estar ordenadas de más
+# reciente a más antiguo. El scraper va a ir recorriendo
+# hasta que alcanzar el límite de días 'max_dias'.
 
 sitios_a_analizar <- data.frame(url1 = c("https://www.bumeran.com.ar",
                                         "https://www.laborum.cl",
@@ -33,15 +33,26 @@ sitios_a_analizar <- data.frame(url1 = c("https://www.bumeran.com.ar",
                                          "Panamá"))
 
 # Creamos la conexión a la db y collection de mongo
-conexionDB <- mongo(collection = "datos", db = "scraperJobs")
+conexionDB <<- mongo(collection = "datos", db = "scraperJobs")
+
 # Definimos una función que limpia texto, y la voy a necesitar seguido
 limpiar_string <- function(string) { return(gsub("^\\s+|\\s+$|\"|\'","",string)) }
-# Definimos la variable 'max_dias' antes descripta
-max_dias <- 1
+
+# Esta función lo que hace es definir la cantidad de días hacia
+# atrás que hay que recorrer. Si 'forzar' es TRUE, se recorre
+# la cantidad de días especificada. Si es FALSE, hace que el
+# recorrido se detenga cuando se llega a la fecha de scrapeo
+# más reciente. Ejemplo: si ayer corrí el script y hoy lo corro
+# otra vez con el parámetro 'días' en, por ej, 10, de estar
+# 'forzar' en FALSE, cuando llegue a la fecha de ayer se detiene.
+fn_max_dias <- function(dias,forzar,pais,nombre) { rtn = dias; if(!forzar) { registros = conexionDB$find(paste0('{"pais": "',pais,'", "sitio_web": "',nombre,'"}')) %>% mutate(Publicado = as.Date(Publicado)); rtn = as.numeric(Sys.Date()-max(registros$Publicado)) }; return(rtn) }
 
 # ~~~~~~~~ Recorrido ~~~~~~~ #
 for(j in 1:nrow(sitios_a_analizar))
 {
+  ### IMPORTANTE: Definimos la variable 'max_dias' antes descripta
+  max_dias <- fn_max_dias(dias = 2,forzar = FALSE,pais = sitios_a_analizar$pais[j], nombre = sitios_a_analizar$nombre[j])
+  
   continuar <- TRUE
   pagina <- 1
   anuncios_procesados <- 0
@@ -64,8 +75,8 @@ for(j in 1:nrow(sitios_a_analizar))
       
       datos_specs <- as.data.frame(matrix(specs_def,nrow=1))
       colnames(datos_specs) <- specs_attr
-      datos_specs$Publicado <- if_else(as.character(datos_specs$Publicado) == "Hoy",Sys.Date(),
-                                       Sys.Date()+1-as.numeric(gsub("[Publicado hace |días]","",datos_specs$Publicado)))
+      datos_specs$Publicado <- suppressWarnings(if_else(as.character(datos_specs$Publicado) == "Hoy",Sys.Date(),
+                                       Sys.Date()+1-as.numeric(gsub("[Publicado hace |días]","",datos_specs$Publicado))))
       
       descripcion <- gsub("[\n|\t]","",limpiar_string(html_text(html_node(html,'div.aviso_description'))))
       
@@ -78,7 +89,7 @@ for(j in 1:nrow(sitios_a_analizar))
                                      empresa = empresa,
                                      descripcion = descripcion),
                           datos_specs)
-        conexionDB$insert(registro)
+        conexionDB$insert(registro %>% na.omit())
       }
       else
       {
